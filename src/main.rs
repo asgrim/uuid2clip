9adb6d7f7f2f8829354487e7b9df41d9e70a3d33
James Titcumb <james@asgrim.com>
2023-10-11 15:40:21 +0100

    send a notification that the UUID is now in the clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use arboard::Clipboard;
 use uuid::Uuid;
+use notify_rust::{Hint, Notification};
 
 fn main() {
     let id = Uuid::new_v4().as_hyphenated().to_string();
-    println!("uuid - {}", id);
+    println!("{}", id);
 
-    Clipboard::new().unwrap().set_text(id).unwrap();
+    Clipboard::new().unwrap().set_text(id.to_owned()).unwrap();
+
+    Notification::new()
+        .summary("Generated UUID in clipboard")
+        .body(id.as_str())
+        .hint(Hint::Transient(true))
+        .show().unwrap();
 }
