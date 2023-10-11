fbdbfac5c10ccfffbd26331d88e5df362fb3b3b8
James Titcumb <james@asgrim.com>
2023-10-11 15:32:09 +0100

    put the uuid in clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use arboard::Clipboard;
 use uuid::Uuid;
 
 fn main() {
-    let id = Uuid::new_v4();
+    let id = Uuid::new_v4().as_hyphenated().to_string();
     println!("uuid - {}", id);
+
+    Clipboard::new().unwrap().set_text(id).unwrap();
 }
